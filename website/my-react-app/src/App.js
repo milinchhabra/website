b848cd9f543793e3c16f95cfa0e00ce4f3a4064c
milin <chhabram26@bcdschool.org>
2024-05-13 19:30:58 -0400

    update

--- website/my-react-app/src/App.js
+++ website/my-react-app/src/App.js
@@ -98,37 +98,49 @@ let tempplayerturn = new PlayerInfo('p1', 'yellow', user1)
     setSquares(squares1)  
   }
 
-  function handleClick(cellx,celly) {
+  function handleClick(cellx, celly) {
     if (!winstate) {
-    let squares1 = structuredClone(squares)
-    let bottomrow = 5
-    let winstate1 = false
-    for(let i = 5;i >= 0; i--) {
-      if (squares1[i][cellx].player == null||squares1[i][cellx].hover === true) {
-        bottomrow=i
-        squares1[bottomrow][cellx].player = playerturn.player
-        squares1[bottomrow][cellx].hover = false
-        winstate1 = CheckWin(squares1[bottomrow][cellx])
-        setTimeout(() => {
-          playClickSound();
-        }, 250);
-        if (!winstate1) {
-          if (playerturn.player === "p1") {
-            setPlayerTurn(new PlayerInfo('p2', 'red', user2))
-          }
-          else {
-            setPlayerTurn(new PlayerInfo('p1', 'yellow', user1))
-          }
-        }
-        setWinState(winstate1)
+      let squaresCopy = structuredClone(squares);
+      let bottomRow = 5;
+      let winstate1 = false;
   
-        setSquares(squares1)  
-        break
+      // Find the bottom-most empty cell in the clicked column
+      for (let i = 5; i >= 0; i--) {
+        if (squaresCopy[i][cellx].player === null || squaresCopy[i][cellx].hover === true) {
+          bottomRow = i;
+          squaresCopy[bottomRow][cellx].player = playerturn.player;
+          squaresCopy[bottomRow][cellx].hover = false;
+          winstate1 = CheckWin(squaresCopy[bottomRow][cellx]);
+  
+          // Play click sound after a short delay
+          setTimeout(() => {
+            playClickSound();
+          }, 250);
+  
+          if (!winstate1) {
+            // Toggle player turn
+            const nextPlayer = playerturn.player === 'p1' 
+              ? new PlayerInfo('p2', 'red', user2) 
+              : new PlayerInfo('p1', 'yellow', user1);
+            setPlayerTurn(nextPlayer);
+          }
+  
+          setWinState(winstate1);
+          setSquares(squaresCopy);
+          break;
+        }
       }
+  
+      // Check for tie if all cells are filled and no winner
+      let tie = CheckTie()  
+      if (tie && !winstate1) {
+        // Set winstate to true for tie
+        setWinState(true);
+        setPlayerTurn(new PlayerInfo("Draw", 'grey', user1));
       }
     }
-
   }
+  
 
   function onEnter(cellx,celly) {
     if (!winstate) {
@@ -169,6 +181,18 @@ let tempplayerturn = new PlayerInfo('p1', 'yellow', user1)
     }
     return squares1 
   }
+
+  function CheckTie() {
+    for(let xi = 5;xi >= 0; xi--) {
+      for(let yi = 6; yi >= 0; yi--) {
+      if(squares[xi][yi].player === null ){
+        return false
+      }
+    }
+  }
+  return true
+  }
+
   function CheckWin(square) {
     //square should equal squares[x][y]
     let win = false;
@@ -223,41 +247,36 @@ let tempplayerturn = new PlayerInfo('p1', 'yellow', user1)
   }
 return (
 
-  <div>
+  <div className='parent'>
+  <div className='board'>
     <div className="board-back" onMouseLeave={onLeave}>
       {squares.map((row, y) => (
         <div key={y}>
           {row.map((cell, x) => (
-          <div className='square-container'>
-            <Square
-              key={`${y}-${x}`}
-              player={cell.player}
-              hover={cell.hover}
-              onSquareClick={() => handleClick(x, y)}
-              Enter={() => onEnter(x, y)} 
-            />
-            <Circle
-              player={cell.player}
-              hover={cell.hover}
-            />
-          </div>
+            <div className='square-container' key={`${y}-${x}`}>
+              <Square
+                player={cell.player}
+                hover={cell.hover}
+                onSquareClick={() => handleClick(x, y)}
+                Enter={() => onEnter(x, y)} 
+              />
+              <Circle
+                player={cell.player}
+                hover={cell.hover}
+              />
+            </div>
           ))}
-          
         </div>
       ))}
-        <div className='board-front'>
-      </div>
+      <div className='board-front'></div>
     </div>
-
-    <div className='player-info'>
-    </div>
-    <Card
-      color={playerturn.color}
-      winner={playerturn.name}
-      winstate = {winstate}
-      onButtonClick={() => restart()}
-
-    ></Card>
   </div>
-  );
-}
+  <div className='player-info'></div>
+  <Card
+    color={playerturn.color}
+    winner={playerturn.name}
+    winstate={winstate}
+    onButtonClick={restart}
+  />
+</div>
+)}
